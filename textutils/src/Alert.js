18b1268134d7fb4b36178b5ce70010b3f04d54bb
Harshit Singh <harshit62512@gmail.com>
2026-02-05 11:04:25 +0530

    changes

--- textutils/src/Alert.js
+++ textutils/src/Alert.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function Alert() {
-    return (
-       <div className="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-  <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-    )
+function Alert(props) {
+  return (
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      {props.Alert}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
 }
